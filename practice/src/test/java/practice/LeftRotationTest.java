64ffe5dbb5047ce07e8c40b0e86b08f598f27d7a
jacob <jacob@MOB700341.hq.k.grp>
2019-09-07 11:52:34 +0530

    Added Junit

---
+++ practice/src/test/java/practice/LeftRotationTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package practice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author jacob
+ *
+ */
+public class LeftRotationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
