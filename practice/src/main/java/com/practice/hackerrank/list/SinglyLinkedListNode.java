1291bac53eba4a868a6583e000d14e4ef80ec2d5
jacob <jacob@MOB700341.hq.k.grp>
2019-09-07 13:11:02 +0530

    Linked list

--- practice/src/main/java/com/practice/hackerrank/list/SinglyLinkedListNode.java
+++ practice/src/main/java/com/practice/hackerrank/list/SinglyLinkedListNode.java
@@ -20,24 +20,4 @@ public class SinglyLinkedListNode {
 		this.data = data;
 	}
 
-	/**
-	 * @param head
-	 * @param data
-	 * @return
-	 */
-	static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
-
-		if (head == null) {
-			head = new SinglyLinkedListNode(data);
-		} else {
-
-			SinglyLinkedListNode node = head;
-			while (node != null && node.next != null) {
-				node = node.next;
-			}
-			node.next = new SinglyLinkedListNode(data);
-		}
-		return head;
-	}
-
 }
