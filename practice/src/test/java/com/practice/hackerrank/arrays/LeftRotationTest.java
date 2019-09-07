1626060f1fe1c2b8303440a9a09e3f5cd80dcc7e
jacob <jacob@MOB700341.hq.k.grp>
2019-09-07 12:14:59 +0530

    Added positive test cases

--- practice/src/test/java/com/practice/hackerrank/arrays/LeftRotationTest.java
+++ practice/src/test/java/com/practice/hackerrank/arrays/LeftRotationTest.java
@@ -25,6 +25,40 @@ public class LeftRotationTest {
 	}
 
 	@Test
+	public void testPositive2() {
+
+		int n = 5;
+		int d = 14;
+		int[] a = { 1, 2, 3, 4, 5 };
+		int[] array = LeftRotation.rotateLeft(n, d, a);
+		int[] b = { 5, 1, 2, 3, 4 };
+		Assert.assertArrayEquals(b, array);
+
+	}
+
+	@Test
+	public void testSame() {
+
+		int n = 5;
+		int d = 10;
+		int[] a = { 1, 2, 3, 4, 5 };
+		int[] array = LeftRotation.rotateLeft(n, d, a);
+		Assert.assertArrayEquals(a, array);
+
+	}
+
+	@Test
+	public void testZero() {
+
+		int n = 5;
+		int d = 0;
+		int[] a = { 1, 2, 3, 4, 5 };
+		int[] array = LeftRotation.rotateLeft(n, d, a);
+		Assert.assertArrayEquals(a, array);
+
+	}
+
+	@Test
 	public void testNull() {
 
 		int n = 5;
