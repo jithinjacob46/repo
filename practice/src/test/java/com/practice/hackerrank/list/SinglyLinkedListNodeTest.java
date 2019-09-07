ad6f400976a386c24a0aade2f33c7505135871f6
jacob <jacob@MOB700341.hq.k.grp>
2019-09-07 13:18:05 +0530

    Insert and delete node

---
+++ practice/src/test/java/com/practice/hackerrank/list/SinglyLinkedListNodeTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.practice.hackerrank.list;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author jacob
+ *
+ */
+class SinglyLinkedListNodeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
