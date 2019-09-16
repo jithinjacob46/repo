88df54fecc69ca6dbcbc8d438a1b0430d638272d
Jithin Jacob <mail2jithinjacob@gmail.com>
2019-09-17 01:55:39 +0530

    Added API Gateway with Lambda proxy integration

--- sample/aws/aws-sam/src/main/java/com/sample/aws/aws_sam/function/HelloWorld.java
+++ sample/aws/aws-sam/src/main/java/com/sample/aws/aws_sam/function/HelloWorld.java
@@ -4,19 +4,24 @@ import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.sample.aws.aws_sam.model.ServerlessOutput;
 
 /**
- * Lambda function that simply prints "Hello World" if the input String is not
- * provided, otherwise, print "Hello " with the provided input String.
+ * Lambda function that returns message "Hello World" if the input name is not
+ * provided, otherwise, print "Hello " with the provided input name.
  */
-public class HelloWorld implements RequestHandler<Map<String, Object>, String> {
+public class HelloWorld implements RequestHandler<Map<String, Object>, ServerlessOutput> {
 
 	@Override
-	public String handleRequest(Map<String, Object> input, Context context) {
+	public ServerlessOutput handleRequest(Map<String, Object> input, Context context) {
 
-		String output = "Hello "
+		String content = "Hello "
 				+ (((input != null && input.containsKey("name"))) ? input.get("name").toString() : "World");
-		context.getLogger().log(output);
+		context.getLogger().log(content);
+
+		ServerlessOutput output = new ServerlessOutput();
+		output.setStatusCode(200);
+		output.setBody(content);
 		return output;
 	}
 
