4b34c230373b02f74e72718584112bed0f9a3592
Jithin Jacob <mail2jithinjacob@gmail.com>
2019-09-16 23:18:29 +0530

    Changed Serverless template from JSON to YAML

--- sample/aws/aws-sam/src/main/java/com/sample/aws/aws_sam/function/HelloWorld.java
+++ sample/aws/aws-sam/src/main/java/com/sample/aws/aws_sam/function/HelloWorld.java
@@ -10,11 +10,14 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
  * provided, otherwise, print "Hello " with the provided input String.
  */
 public class HelloWorld implements RequestHandler<Map<String, Object>, String> {
+
 	@Override
 	public String handleRequest(Map<String, Object> input, Context context) {
-		String name = ((input != null && input.containsKey("name"))) ? input.get("name").toString() : null;
-		String output = "Hello " + ((name != null && !name.isEmpty()) ? name : "World");
+
+		String output = "Hello "
+				+ (((input != null && input.containsKey("name"))) ? input.get("name").toString() : "World");
 		context.getLogger().log(output);
 		return output;
 	}
+
 }
