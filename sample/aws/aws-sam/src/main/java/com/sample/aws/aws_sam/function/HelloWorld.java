9616951b29cee629c0cfd5046cedf07cf5a953af
Jithin Jacob <mail2jithinjacob@gmail.com>
2019-09-16 23:01:50 +0530

    Read input from JSON object

--- sample/aws/aws-sam/src/main/java/com/sample/aws/aws_sam/function/HelloWorld.java
+++ sample/aws/aws-sam/src/main/java/com/sample/aws/aws_sam/function/HelloWorld.java
@@ -1,17 +1,20 @@
 package com.sample.aws.aws_sam.function;
 
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 /**
- * Lambda function that simply prints "Hello World" if the input String is not provided,
- * otherwise, print "Hello " with the provided input String.
+ * Lambda function that simply prints "Hello World" if the input String is not
+ * provided, otherwise, print "Hello " with the provided input String.
  */
-public class HelloWorld implements RequestHandler<String, String> {
-    @Override
-    public String handleRequest(String input, Context context) {
-        String output = "Hello " + ((input != null && !input.isEmpty()) ? input : "World");
-        context.getLogger().log(output);
-        return output;
-    }
+public class HelloWorld implements RequestHandler<Map<String, Object>, String> {
+	@Override
+	public String handleRequest(Map<String, Object> input, Context context) {
+		String name = ((input != null && input.containsKey("name"))) ? input.get("name").toString() : null;
+		String output = "Hello " + ((name != null && !name.isEmpty()) ? name : "World");
+		context.getLogger().log(output);
+		return output;
+	}
 }
